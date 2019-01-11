5109b7c01ce8de96775ecb04028363491a5c2c03
Rathes Sachchithananthan <sachchi@rathes.de>
2019-01-11 23:54:38 +0100

    ✨ Validate given API version

--- src/ChargeBee.php
+++ src/ChargeBee.php
@@ -2,8 +2,12 @@
 
 namespace Chargebee;
 
+use InvalidArgumentException;
+
 class ChargeBee
 {
+    const ALLOWED_API_VERSIONS = ['v1', 'v2'];
+
     /**
      * @var string
      */
@@ -23,6 +27,11 @@ class ChargeBee
     {
         $this->site = $site;
         $this->apiKey = $apiKey;
+
+        if (!in_array($apiVersion, self::ALLOWED_API_VERSIONS)) {
+            throw new InvalidArgumentException("$apiVersion is not a valid API version.");
+        }
+
         $this->apiVersion = $apiVersion;
     }
 
