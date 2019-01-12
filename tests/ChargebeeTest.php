a8f229c64a253427f6f76e2baf0677f255545e6d
Rathes Sachchithananthan <sachchi@rathes.de>
2019-01-12 01:36:39 +0100

    ✅ Verify it sends a simple get request

--- tests/ChargebeeTest.php
+++ tests/ChargebeeTest.php
@@ -3,6 +3,13 @@
 namespace Chargebee\Test;
 
 use Chargebee\ChargeBee;
+use Chargebee\Response;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
@@ -31,5 +38,37 @@ final class ChargebeeTest extends TestCase
 
         $this->expectException(InvalidArgumentException::class);
         new Chargebee('siteName', 'api-key', 'v3');
+    }
+
+    /**
+     * @test
+     */
+    public function itSendsAGetRequest()
+    {
+        $mock = new MockHandler([
+            new GuzzleResponse(200, ['X-Foo' => 'Bar'], json_encode(['test' => 'test2'])),
+        ]);
+
+        $container = [];
+        $history = Middleware::history($container);
+
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+
+        $httpClient = new Client([
+            'handler' => $stack,
+        ]);
+
+        $client = new ChargeBee('site', 'api-key', 'v2', $httpClient);
+
+        $response = $client->get('/subscription');
+
+        /** @var Request $request */
+        $request = $container[0]['request'];
+
+        $this->assertEquals('/api/v2/subscription', $request->getUri()->getPath());
+        $this->assertEquals('site.chargebee.com', $request->getUri()->getHost());
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(['test' => 'test2'], $response->getParsedBody());
     }
 }
